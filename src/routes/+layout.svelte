5914aac3b4145600877a3bd10731e3843ab65004
redfe <tetsu999@gmail.com>
2024-12-05 21:45:07 +0900

    view transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,16 @@
 <script lang="ts">
+	import { onNavigate } from '$app/navigation';
 	const { children } = $props();
+	onNavigate((navigation) => {
+		if (!document.startViewTransition) return;
+
+		return new Promise((resolve) => {
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
 </script>
 
 <header>
